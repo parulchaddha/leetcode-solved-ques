01b11e4af7748ce6328f7a0088143183a074325e
parulchaddha <106451107+parulchaddha@users.noreply.github.com>
2022-08-22 07:57:36 +0530

    Time: 7 ms (6.65%), Space: 6.4 MB (52.99%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> arr;
+    
+    int solve(int m , int n , int i , int j){
+        if(i==m-1 || j==n-1)
+            return 1;
+        if(i>m-1 || j >n-1)
+            return 0;
+        if(arr[i][j]!=-1)
+            return arr[i][j];
+        return arr[i][j]=solve(m,n,i+1,j)+solve(m,n,i,j+1);
+    }
+    int uniquePaths(int m, int n) {
+       arr.resize(m,vector<int>(n,-1));
+       int x=solve(m,n,0,0);
+        return x;
+    }
+};
